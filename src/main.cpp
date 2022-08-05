979aa25b6a5f2d9d893f5ca2c1de86abd91197b7
Ryuu22 <daniicarmona22@gmail.com>
2022-08-05 13:14:24 +0200

    Added everything :)

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "raylib.h"
+
+int main(int argc, const char** argv) {
+    int screenWidth = 512;
+    int screenHeight = 512;
+
+    InitWindow(screenWidth, screenHeight, "Tiny dungeon");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        // Update Here
+
+        BeginDrawing();
+        {
+            ClearBackground(BLACK);
+            DrawText("My new game", 10, 10 , 10, WHITE);
+        }
+        EndDrawing();
+    }   
+
+    CloseWindow();
+    return 0;
+}
